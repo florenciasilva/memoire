d4278b6cd21cd2c3a2811248c154c9f771274ef6
florenciasilva <hensgrej@gmail.com>
2019-09-17 12:52:19 -0500

    Add edit with firestore, keyboard support for dynamic element

--- src/Components/AllPosts.js
+++ src/Components/AllPosts.js
@@ -7,9 +7,13 @@ class AllPosts extends Component{
         super();
         this.state = {
             msg: [],
-            isLoading: true
-        }
-    }
+            isLoading: true,
+            hidden: true,
+            currentEdit: -1,
+            editText: '',
+            tabIndex: '-1'
+        };
+    };
 
 
     handleChange = (e) => {
@@ -39,12 +43,37 @@ class AllPosts extends Component{
         });
     };
 
+    handleEdit = (e, i, post) => {
+        e.target['aria-expanded'] = true;
+        this.setState({currentEdit: i,
+                       editText: post,
+                       tabIndex: '0'});
+    }
+
+    editStage = (e) => {
+        this.setState({editText: e.target.value})
+    }
+
+    handleSubmit = (e, id) => {
+        const db = firebase.firestore();
+        this.setState({ currentEdit: -1})
+        return db.collection('posts').doc(id).update({
+            post: this.state.editText,
+            lastEdit: new Date().toString()
+        })
+        .then(function() {
+            console.log("Document successfully updated!");
+        })
+        .catch(function(error) {
+            console.error("Error updating document: ", error);
+        });
+    }
+
     componentDidMount() {
         this.getPosts();
     };
 
     render(){
-        //console.log(this.state)
         if(this.state.isLoading) {
             return (
                 <p>cargando</p>
@@ -54,14 +83,26 @@ class AllPosts extends Component{
                 return (
                     <article key={i}>
                         <p>{post.data().author}</p>
-                        <p>{post.data().post}</p>
+                        <p  style={{display: (i === this.state.currentEdit) ? "none" : "block"}} >{post.data().post}</p>
+                        <div style={{display: (i === this.state.currentEdit) ? "block" : "none"}} hidden={this.state.hidden} id="edit-section" role="region" tabIndex={this.state.tabIndex}>
+                            <label htmlFor="edit">Editar post</label>
+                            <input
+                                ref={input => input && input.focus()}
+                                type="text"
+                                id="edit"
+                                value={this.state.editText}
+                                onChange={ e => this.editStage(e)} />
+                            <button onClick={e => this.handleSubmit(e, post.id)}>Editar</button>
+                        </div>
                         <p>{post.data().date}</p>
                         <button onClick={() => this.handleDelete(post.id)}>Borrar</button>
+                        <button aria-controls="t1" aria-expanded="false" onClick={e => this.handleEdit(e, i, post.data().post)}>Editar</button>
                     </article>
                 );
             });
         };
     };
 };
+
 
 export default AllPosts;
